bfae4333193e9df47e340932bfe544f84d39a905
Ankit <bestfreeserver19@gmail.com>
2025-10-19 14:39:37 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,43 @@
+# PASTE ALL
+echo "
+=============================================================
+
+                   Gamerzhacker
+                  Maded By Gamerzhacker
+             Copyright (C) By Gamerzhacker
+             
+
+               VM V1 By Gamerhacker
+
+      🧊 ---- [       GVM PANEL ] ---- 🧊
+
+============================================================
+"
+
+git clone https://github.com/Ankitboss7/Gvm-Panel.git
+apt install unzip -y && unzip Gvm.py && unzip templates
+cd Gvm-Panel
+apt update
+apt install python3 -y
+apt install neofetch -y && neofetch
+
+apt install python3-pip  
+python3 -m pip install --upgrade pip
+echo "
+=========================================================
+
+                    GVM STARTING IN 5 Secoands
+                      Admin:
+                     Username: admin
+                     Password: admin
+
+                    Check Port (" 3000 ")
+
+                      Copyright (C) By Gamerzhacker
+==========================================================
+
+"
+
+pip install flask flask-socketio flask-login docker paramiko psutil python-dotenv email-validator flask_limiter
+# FINAL
+python3 Gvm.py
